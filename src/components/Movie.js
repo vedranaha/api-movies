99a0a51cd4c2f181c93611b047d072595182ea6b
Vedranaha <vedranaha@gmail.com>
2021-09-01 09:10:29 +0200

    average, original title

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,15 +2,23 @@ import React from "react";
 
 const Movie = ({ movie }) => {
   return (
-    <div className="movie" onclick="getMovieDetail(${movie.id})">
+    <div className="movie">
       <div className="title">
-        <h2>{movie.title}</h2>
+        <h3>{movie.title}</h3>
       </div>
       <div className="imgMovie">
         <img
           src={"http://image.tmdb.org/t/p/w185" + movie.poster_path}
-          alt="movie image"
+          alt="movie img"
         />
+        <div className="average">
+          <p>{movie.vote_average}/10</p>
+        </div>
+      </div>
+      <div className="moreDetails">
+        <h4 className="originalTitle">{movie.original_title}</h4>
+        <span>({movie.release_date})</span>
+        <h5> Sinopsis:</h5> <p>{movie.overview}</p>
       </div>
     </div>
   );
